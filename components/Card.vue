acf6526d4c43d08709da11f549514e97f51b6b4c
Heleno Salgado <73316162+HelenoSalgado@users.noreply.github.com>
2024-07-03 01:00:42 -0300

    size image

--- components/Card.vue
+++ components/Card.vue
@@ -3,7 +3,7 @@ defineProps(['title', 'description', 'src']);
 </script>
 <template>
     <div class="card">
-        <NuxtImg format="webp" loading="lazy" :src :alt="title"></NuxtImg>
+        <NuxtImg format="webp" loading="lazy" width="300px" height="200px" :src :alt="title"></NuxtImg>
         <div>
             <h3>{{ title }}</h3>
             <p>{{ description }}</p>
